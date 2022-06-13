8427aac0224e5021e09d6c8281544aa98641d9d3
Alon Ronder <68230416+Alpha110R@users.noreply.github.com>
2022-06-14 01:31:35 +0300

    Update the barNavigation icons when choosing and add amounts to categories

--- app/src/main/java/com/example/alpha_care/PetProfileActivity.java
+++ app/src/main/java/com/example/alpha_care/PetProfileActivity.java
@@ -2,13 +2,32 @@ package com.example.alpha_care;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.ColorStateList;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
 public class PetProfileActivity extends AppCompatActivity {
+    private FloatingActionButton fab_walk, fab_food, fab_groom;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        findViews();
+        fab_walk.setOnClickListener(view -> {
+            fab_walk.setBackgroundTintList(ColorStateList.valueOf(Color.parseColor("#397D54")));
+            //fab_walk.setImageResource(R.drawable.ic_dogwalking_full);
+            fab_walk.setImageTintList(ColorStateList.valueOf(Color.parseColor("#73CD88")));
+        });
+
+    }
+
+    private void findViews() {
+        fab_walk = findViewById(R.id.fab_walk);
+        fab_food = findViewById(R.id.fab_food);
+        fab_groom = findViewById(R.id.fab_groom);
     }
 }
